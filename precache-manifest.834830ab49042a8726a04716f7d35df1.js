37389ee209e94152ac5d83163368d1c0d6e63b81
Abdullah-Git149 <keepcool200@gmail.com>
2020-11-14 14:47:30 +0500

    Updates

---
+++ precache-manifest.834830ab49042a8726a04716f7d35df1.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d30d1dba27f427e1140b98c000c2c834",
+    "url": "/Olx-ReactApp/index.html"
+  },
+  {
+    "revision": "6c3334b37708a40c9f25",
+    "url": "/Olx-ReactApp/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "481b7307ef979016ce63",
+    "url": "/Olx-ReactApp/static/css/main.2710adf2.chunk.css"
+  },
+  {
+    "revision": "6c3334b37708a40c9f25",
+    "url": "/Olx-ReactApp/static/js/2.0f1ea5f5.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/Olx-ReactApp/static/js/2.0f1ea5f5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "481b7307ef979016ce63",
+    "url": "/Olx-ReactApp/static/js/main.0371e129.chunk.js"
+  },
+  {
+    "revision": "23ad19b3921b04be5b17",
+    "url": "/Olx-ReactApp/static/js/runtime-main.75a8df8c.js"
+  },
+  {
+    "revision": "78ff677793590484811bcca6a3fb1234",
+    "url": "/Olx-ReactApp/static/media/Capture.78ff6777.PNG"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Olx-ReactApp/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "8505686db886f8db41a5cfe81b001caf",
+    "url": "/Olx-ReactApp/static/media/pic1.8505686d.PNG"
+  },
+  {
+    "revision": "ee60171452c8eddc81635b539d199b30",
+    "url": "/Olx-ReactApp/static/media/pic2.ee601714.jpg"
+  }
+]);
